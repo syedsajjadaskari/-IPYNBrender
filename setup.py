15be3d0db384df578518454d52295cec6f502d32
Syed Sajjad Askari <84753561+syedsajjadaskari@users.noreply.github.com>
2023-01-31 18:56:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.2"
+__version__ = "0.0.02"
 
 REPO_NAME = "IPYNBrender "
 AUTHOR_USER_NAME = "syedsajjadaskari"
